d2372b8dcdfdc58c0c79ac7534a9adf969d3deb5
Martin Kabanen <martin.kabanen@gmail.com>
2024-10-24 22:37:53 +0300

    refactor: email masking

---
+++ scripts/email.js
@@ -0,0 +1,2 @@
+document.getElementById('email-link').href = atob('bWFpbHRvOm1hcnRpbi5rYWJhbmVuQGdtYWlsLmNvbQ==');
+document.getElementById('email-image').alt = atob('bWFydGluLmthYmFuZW5AZ21haWwuY29t');
