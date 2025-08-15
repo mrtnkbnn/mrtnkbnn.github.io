5b458e365635545aa94d3e61cf2fd637a10ff31c
Martin Kabanen <martin.kabanen@gmail.com>
2025-08-15 18:54:36 +0300

    feat: qr

--- scripts/email.js
+++ scripts/email.js
@@ -1,2 +1,1 @@
 document.getElementById('email-link').href = atob('bWFpbHRvOm1hcnRpbi5rYWJhbmVuQGdtYWlsLmNvbQ==');
-document.getElementById('email-image').alt = atob('bWFydGluLmthYmFuZW5AZ21haWwuY29t');
